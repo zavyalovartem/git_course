51ab4fcaf34f5dad270a85191ff295d6e601953a
juliatimoshenkova@gmail.com <juliatimoshenkova@gmail.com>
2021-09-19 22:04:27 +0500

    add button

--- calculate/calculateGUI.py
+++ calculate/calculateGUI.py
@@ -2,4 +2,21 @@ from tkinter import *
 
 
 class Main(Frame):
-    
+    def __init__(self, root):
+        super(Main, self).__init__(root)
+        self.build()
+
+    def build(self):
+        self.formula = "0"
+        self.lbl = Label(text=self.formula, font=("Times New Roman", 21, "bold"), bg="#000", foreground="#FFF")
+        self.lbl.place(x=11, y=50)
+
+        btns = [
+            "C", "DEL", "*", "=",
+            "1", "2", "3", "/",
+            "4", "5", "6", "+",
+            "7", "8", "9", "-",
+            "(", "0", ")", "X^2"
+        ]
+
+        
